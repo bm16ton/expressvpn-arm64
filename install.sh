a5aa7c1975d56df22f51c3b7ebe84a09b64b5350
bm16ton <bm16ton@gmail.com>
2021-01-13 10:16:21 -0500

    update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ sudo cp cur-server /usr/local/bin/
 sudo cp expvpn /usr/local/bin/
 sudo cp expvpn-gtk /usr/local/bin/
 sudo cp expvpn-gtk3 /usr/local/bin/
+sudo cp expvpn-setup /usr/local/bin/
 sudo chmod +x  /usr/local/bin/expvpn
 sudo chmod +x  /usr/local/bin/expvpn-gtk
 sudo chmod +x  /usr/local/bin/expvpn-gtk3
 sudo chmod +x  /usr/local/bin/cur-server
-mkdir /usr/share/expvpn
+sudo chmod +x  /usr/local/bin/expvpn-setup
+mkdir /usr/share/expvpn 2> /dev/null
 sudo cp server-list-udp-url-port /usr/share/expvpn/
 

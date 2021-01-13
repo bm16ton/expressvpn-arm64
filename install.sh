c601c9025be47746bec399fdaeb3289115d461b2
bm16ton <bm16ton@gmail.com>
2021-01-13 06:00:47 -0500

    update install for expvpn-gtk3

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 sudo cp cur-server /usr/local/bin/
 sudo cp expvpn /usr/local/bin/
 sudo cp expvpn-gtk /usr/local/bin/
+sudo cp expvpn-gtk3 /usr/local/bin/
 sudo chmod +x  /usr/local/bin/expvpn
 sudo chmod +x  /usr/local/bin/expvpn-gtk
+sudo chmod +x  /usr/local/bin/expvpn-gtk3
 sudo chmod +x  /usr/local/bin/cur-server
 mkdir /usr/share/expvpn
 sudo cp server-list-udp-url-port /usr/share/expvpn/
